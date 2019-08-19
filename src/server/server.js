5abe2fcd5cea4d53b172cfa8c877c4ce88115054
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2019-08-19 12:25:03 -0300

    Server com express

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,22 @@
 const http = require('http');
+const express = require('express');
+
+const app = express();
+
 const port = 3000;
 const state = ['stop', 'play'];
 let server;
+
+app.get("/", function(req, res) {
+    res.send('Hello World!');
+});
 
 const getNextState = () => {
     return state.reverse()[0];
 };
 
 const playServer = () => {
-    server = http.createServer((req, resp) => {
-        resp.end("Hello");
-    }).listen(port);
+    server = app.listen(port);
 };
 
 const stopServer = () => {
